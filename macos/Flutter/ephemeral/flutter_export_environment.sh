ad282b339f3f93d7320182f06c663c1be3bc3603
Sam Allen <sokunsamnang45@gmail.com>
2022-06-26 04:56:16 +0700

    push

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Volumes/NewPart/flutter"
-export "FLUTTER_APPLICATION_PATH=/Volumes/Project/bitriel"
+export "FLUTTER_ROOT=/home/samallen/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/samallen/Documents/bitriel"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=5.0.0"
-export "FLUTTER_BUILD_NUMBER=80"
+export "FLUTTER_BUILD_NAME=5.0.1"
+export "FLUTTER_BUILD_NUMBER=81"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=false"
 export "TREE_SHAKE_ICONS=false"
