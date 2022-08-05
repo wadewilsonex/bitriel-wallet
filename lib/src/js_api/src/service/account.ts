5074c6675eea92d19f98d263a1d96ecf4c2487fd
DaveatCor <condaveat@gmail.com>
2022-08-05 10:23:16 +0700

    change project to mainnet, 05/Aug/22

--- lib/src/js_api/src/service/account.ts
+++ lib/src/js_api/src/service/account.ts
@@ -8,7 +8,7 @@ import { Keyring } from "@polkadot/keyring";
 import { ApiPromise } from "@polkadot/api";
 
 import { subscribeMessage } from "./setting";
-let isMainnet = Boolean(false);
+let isMainnet = Boolean(true);
 let testNet = 204;
 let mainnet = 972;
 let keyring = new Keyring({ ss58Format: isMainnet ? mainnet : testNet, type: "sr25519" });
