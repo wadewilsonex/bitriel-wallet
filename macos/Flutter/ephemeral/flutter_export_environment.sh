c2ad8db71938aa6328cd0f64f74a2f2f78bac813
DaveatCor <condaveat@gmail.com>
2022-08-08 14:14:52 +0700

    change gradle 7.3.1. -> 7.30-all

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\Daveat\Documents\dev_en\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Daveat\Documents\bitriel"
+export "FLUTTER_ROOT=/home/daveat/Documents/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/daveat/Documents/bitriel-wallet"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=5.1.0"
-export "FLUTTER_BUILD_NUMBER=94"
+export "FLUTTER_BUILD_NUMBER=95"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=false"
 export "TREE_SHAKE_ICONS=false"
