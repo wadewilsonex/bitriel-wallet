ddfac90404053356b41fcf8ca630174dd58056b9
DaveatCor <condaveat@gmail.com>
2023-05-20 09:53:22 +0700

    override from bitriel-2, 20/May/23

--- lib/src/constants/asset_path.dart
+++
@@ -1,6 +0,0 @@
-import 'package:wallet_apps/index.dart';
-
-class AssetPath {
-
-  static String contractJson = "${AppConfig.assetsPath}json/supported_contract.json";
-}
--- lib/src/service/exception_handler.dart
+++
@@ -1,6 +0,0 @@
-class ExceptionHandler implements Exception{
-
-  String cause;
-
-  ExceptionHandler(this.cause);
-}
