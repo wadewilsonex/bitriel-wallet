59b796c5f1a01a69a274fcf6c7589833c5633644
DaveatCor <condaveat@gmail.com>
2023-07-25 13:05:55 +0700

    working on send trx, 25/Jul/23

---
+++ lib/presentation/screen/wallet/send_trx_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SendTrxScreen extends StatelessWidget {
+  const SendTrxScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
